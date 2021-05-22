2356627c61b3d778bd05f14eceb239aadbe804e7
vkuna <kuna.vaclav@gmail.com>
2021-05-23 00:28:09 +0200

    Wrapped event
    Signed-off-by: vkuna <kuna.vaclav@gmail.com>

--- params.py
+++ params.py
@@ -3,13 +3,14 @@ This file sets parameters used in data dump OpenEEW algorithm
 """
 
 # MQTT
-MQTT = "local"  # local, custom, or IBM
+MQTT = "IBM"  # local, custom, or IBM
 
 # PARAMETERS
-region = "MX"
+region = "OE"
 event_table_name = "openeew-events"
 detection_table_name = "openeew-detections"
-sleep_time_save = 2
+sleep_time_save = 10
+max_pause = 10  # save event if there are no entries for this time
 
 # TEST DATA PATH
 test_data_path = "../data/"
@@ -22,4 +23,5 @@ params = {
     "sleep_time_save": sleep_time_save,
     "MQTT": MQTT,
     "test_data_path": test_data_path,
+    "max_pause": max_pause,
 }
